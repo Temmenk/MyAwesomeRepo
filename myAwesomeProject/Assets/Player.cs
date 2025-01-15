fef988972362a86b50baedb809d10aa3de57fadf
Temmenk <temmenk@my.erau.edu>
2025-01-15 13:40:47 -0700

    play speed adjusted

--- myAwesomeProject/Assets/Player.cs
+++ myAwesomeProject/Assets/Player.cs
@@ -13,22 +13,22 @@ public class Player : MonoBehaviour
     {
        if (Input.GetKey(KeyCode.W))
        {
-            transform.position += Vector3.forward *.20f;
+            transform.position += Vector3.forward *.30f;
        } 
 
        if (Input.GetKey(KeyCode.S))
        {
-            transform.position += Vector3.back *.20f;
+            transform.position += Vector3.back *.30f;
        }
         
         if (Input.GetKey(KeyCode.A))
        {
-            transform.position += Vector3.left *.20f;
+            transform.position += Vector3.left *.30f;
        } 
 
         if (Input.GetKey(KeyCode.D))
        {
-            transform.position += Vector3.right *.20f;
+            transform.position += Vector3.right *.30f;
        } 
     }
 }
